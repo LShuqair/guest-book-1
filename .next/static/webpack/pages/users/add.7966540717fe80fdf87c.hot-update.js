e37222c1e7e38d0ccf293cddd72894daa4e1dd67
Luna Shuqair <luna.shuqair89@gmail.com>
2021-10-28 11:17:40 -0400

    udate

---
+++ .next/static/webpack/pages/users/add.7966540717fe80fdf87c.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/users/add",{
+
+/***/ "./data/users.json":
+/*!*************************!*\
+  !*** ./data/users.json ***!
+  \*************************/
+/*! exports provided: 0, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"[{\\\"name\\\":\\\"qa\\\",\\\"message\\\":\\\"a\\\",\\\"id\\\":1,\\\"dateCreated\\\":\\\"2021-10-28T15:16:40.036Z\\\",\\\"dateUpdated\\\":\\\"2021-10-28T15:16:40.036Z\\\"}]\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL2RhdGEvdXNlcnMuanNvbi5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./data/users.json\n");
+
+/***/ })
+
+})
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # guest-book-1
+
+ A Simple Guest-Book application using Next.js
